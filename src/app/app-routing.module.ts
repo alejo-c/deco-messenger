12c376a7ad2eef7baa45ad5c26584af3c336d762
alejandro-castrillon <4lejo.castrillon@gmail.com>
2020-05-22 16:28:38 -0500

    Create app routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+
+import { HomeComponent } from './pages/home/home.component'
+import { SigninComponent } from './pages/signin/signin.component'
+import { SignupComponent } from './pages/signup/signup.component'
+
+const routes: Routes = [
+	{ path: 'home', component: HomeComponent },
+	{ path: 'signin', component: SigninComponent },
+	{ path: 'signup', component: SignupComponent },
+	{ path: '**', pathMatch: 'full', redirectTo: '/home' }
+]
+
+@NgModule({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
+})
+export class AppRoutingModule { }
