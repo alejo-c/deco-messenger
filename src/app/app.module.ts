1052b2779d4f9b090c15a43f3b18154c9620ea6f
alejandro-castrillon <4lejo.castrillon@gmail.com>
2020-05-22 16:45:44 -0500

    Import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module'
+import { MDBBootstrapModule } from 'angular-bootstrap-md'
+
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		MDBBootstrapModule.forRoot(),
+		ComponentsModule,
+		PagesModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
