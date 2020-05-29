c307249a720b6f2771b9aa762f9a5ca17cc9fd99
alejandro-castrillon <4lejo.castrillon@gmail.com>
2020-05-29 01:25:28 -0500

    Implement the preview chats list

--- src/app/components/chat-list/chat-list.component.ts
+++ src/app/components/chat-list/chat-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/User';
 import { Chat } from 'src/app/models/Chat';
+import { Message } from 'src/app/models/Message';
 
 @Component({
 	selector: 'app-chat-list',
@@ -10,14 +12,27 @@ import { Chat } from 'src/app/models/Chat';
 })
 export class ChatListComponent implements OnInit {
 
+	public user: User
 	public chats: Chat[]
 	public currentChat: Chat
+	public messages: Message[]
 
 	constructor(private userService: UserService) {
-		this.chats = this.userService.user.chats
+		this.user = this.userService.user
+		this.chats = this.user.chats
+		console.log('current:', this.currentChat)
 	}
 
 	ngOnInit(): void {
 	}
 
+	openChat(chat: Chat) {
+		this.messages = chat.messages.sort(
+			(a: Message, b: Message) => {
+				return new Date(a.datetime).getTime() - new Date(b.datetime).getTime()
+			}
+		)
+		this.currentChat = chat
+		console.log('current:', this.currentChat)
+	}
 }
